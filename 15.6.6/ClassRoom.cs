a589879f4322e5473d9c609bd5965b12b1cb27eb
Roman Sachuck <roman.sachuk.1997@mail.ru>
2024-01-24 17:25:56 +0300

    .

---
+++ 15.6.6/ClassRoom.cs
@@ -0,0 +1,7 @@
+﻿namespace Task1
+{
+    public class ClassRoom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
---
+++ 15.6.6/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new ClassRoom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new ClassRoom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new ClassRoom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(ClassRoom[] classes)
+        {
+            return (from classRoom in classes
+                    from student in classRoom.Students
+                    select student).ToArray();
+        }
+    }
+}
